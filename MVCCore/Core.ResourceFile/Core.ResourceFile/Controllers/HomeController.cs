6a714fb25873218390985e40f4b046b5b83b0333
Nhân Lê <nhan.lt@aris-vn.com>
2019-02-21 16:54:32 +0700

    update Core.ResourceFile

---
+++ MVCCore/Core.ResourceFile/Core.ResourceFile/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Localization;
+
+
+namespace Core.ResourceFile.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}

662143f177df91e49d120a2d170161cee515e137
Dong Cong Danh <danhrz@gmail.com>
2019-02-22 14:05:49 +0700

    Finish EF.SqlServer?

---
+++ EntityCore/EF.SqlServer/EF.SqlServer/Migrations/20190222054525_InitialCreate.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace EF.SqlServer.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    CustomerId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    JoinDate = table.Column<DateTime>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    Phone = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.CustomerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    MovieId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    MovieId1 = table.Column<int>(nullable: true),
+                    Title = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.MovieId);
+                    table.ForeignKey(
+                        name: "FK_Movies_Movies_MovieId1",
+                        column: x => x.MovieId1,
+                        principalTable: "Movies",
+                        principalColumn: "MovieId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movies_MovieId1",
+                table: "Movies",
+                column: "MovieId1");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
---
+++ EntityCore/EF.SqlServer/EF.SqlServer/Models/Model.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System;
+
+namespace TestDb.Models
+{
+    public class TheaterContext : DbContext
+    {
+        public TheaterContext(DbContextOptions<TheaterContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+
+    public class Movie
+    {
+        [Key]
+        public int MovieId { get; set; }
+
+        [Required(ErrorMessage = "Movie need name.")]
+        public string Title { get; set; }
+
+        public ICollection<Movie> Movies { get; set; }
+    }
+
+    public class Customer
+    {
+        [Key]
+        public int CustomerId { get; set; }
+
+        [Required(ErrorMessage = "Customer need name.")]
+        public string Name { get; set; }
+
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString ="{0: dd-MM-yyyy}")]
+        [DataType(DataType.Date)]
+        public DateTime JoinDate { get; set; }
+
+        public string Phone { get; set; }
+    }
+}

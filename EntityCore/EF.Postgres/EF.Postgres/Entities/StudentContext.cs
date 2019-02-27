d87642ad900409efbcb651bd39c515fd465ac303
Unknown <nhinxungquanhminh@gmail.com>
2019-02-27 17:46:30 +0700

    migration to Postgre

---
+++ EntityCore/EF.Postgres/EF.Postgres/Entities/StudentContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EF.Postgres.Entities
+{
+    public class StudentContext: DbContext
+    {
+        public StudentContext(DbContextOptions options): base(options) { }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}

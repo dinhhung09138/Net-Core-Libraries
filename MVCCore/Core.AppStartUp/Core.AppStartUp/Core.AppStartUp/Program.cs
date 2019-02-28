97c67b1a63cc0509c8237f76c1f3e94d677d9b0a
Trung Nguyen <Trungghost.win@gmail.com>
2019-02-28 13:41:32 +0700

    Refs #4 programm.cs and startup.cs demo

---
+++ MVCCore/Core.AppStartUp/Core.AppStartUp/Core.AppStartUp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Core.AppStartUp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // call funcion CreateWebHostBuilder to create hosting
+            CreateWebHostBuilder(args)
+                .Build()
+                .Run();
+        }
+
+        // config hosting
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>(); // call to Startup.cs page    
+    }
+}

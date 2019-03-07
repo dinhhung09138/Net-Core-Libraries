48878953fff7b3233d2c833ebc809f7b81be9a97
DESKTOP-QQ2II3P\NhanLe <lenhanbz@gmail.com>
2019-03-07 07:35:46 +0700

    refs #6

--- MVCCore/Core.AppState/Core.AppState/Controllers/BookController.cs
+++ MVCCore/Core.AppState/Core.AppState/Controllers/BookController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.AppState.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Core.AppState.Views
@@ -10,11 +11,28 @@ namespace Core.AppState.Views
     {
         public IActionResult Create()
         {
+           
+
+
             TempData["StatusMessage"] = "Create book successfully";
+
             return View();
         }
         public IActionResult Index()
         {
+            ViewData["Gree"] = "Hello I am ViewData ";
+            ViewData["Address"] = new Address()
+            {
+                name = "Nam",
+                city = "HaNoi",
+            };
+
+            ViewBag.Greeting = "Hello I am ViewBag ";
+            ViewBag.Address = new Address()
+            {
+                name = "Nam",
+                city = "HaNoi",
+            };
             return View();
         }
         public IActionResult Index2()
---
+++ MVCCore/Core.AppState/Core.AppState/Models/Address.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.AppState.Models
+{
+    public class Address
+    {
+        public String name { get; set; }
+        public String city { get; set; }
+    }
+}
